d1b0694eb2cae0c4b79fa8fc54d652f3d6b1bf6f
giGurylsPro <71938208+giGurylsPro@users.noreply.github.com>
2020-09-26 19:44:32 +0300

    Update fripartner.sh

--- fripartner.sh
+++ fripartner.sh
@@ -3,25 +3,25 @@
 while true; do
     read -p "on which machine are you installing linode, if L Vultr, Press V..." vl
     case $vl in
-        [Vv]* ) wget https://cdn.discordapp.com/attachments/750065676391022672/750065726219485234/vultur.sh; break;;
-        [Ll]* ) wget https://cdn.discordapp.com/attachments/750065676391022672/750065722499137616/inter1.sh; break;;
-        * ) echo "Lütfen birine basiniz L veya V.";;
+        [Vv]* ) wget --no-check-certificate https://raw.githubusercontent.com/giGurylsPro/best-friends/master/frivul.sh; break;;
+        [Ll]* ) wget --no-check-certificate https://raw.githubusercontent.com/giGurylsPro/best-friends/master/frilin.sh; break;;
+        * ) echo "Please press one L or V.";;
     esac
 done
 END
 
-chmod +x vultur.sh
+chmod +x frivul.sh
 
-sed -i -e 's/\r$//' vultur.sh
-
-cd /root
-
-./vultur.sh
-
-chmod +x inter1.sh
-
-sed -i -e 's/\r$//' inter1.sh
+sed -i -e 's/\r$//' frivul.sh
 
 cd /root
 
-./inter1.sh
+./frivul.sh
+
+chmod +x frilin.sh
+
+sed -i -e 's/\r$//' frilin.sh
+
+cd /root
+
+./frilin.sh
